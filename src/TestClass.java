7324ee9372a62a096c7fd812916ec561852a7149
Dibyam <dibyam1101@gmail.com>
2021-07-05 21:05:56 +0530

    fixed typo

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("This is a CestClass!!");
+        System.out.println("This is a TestClass!!");
     }
 
     static void function(){
