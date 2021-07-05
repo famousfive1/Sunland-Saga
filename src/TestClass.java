693e0a7e72805f74bee6be3895f1d66db49e3fab
Dibyam <dibyam1101@gmail.com>
2021-07-05 21:05:27 +0530

    Function Added

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,4 +2,8 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("This is a CestClass!!");
     }
+
+    static void function(){
+        System.out.println("Function");
+    }
 }
