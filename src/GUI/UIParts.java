12df2d96f7d82dad8e034f7812c51f7d088ad0de
BuildTools <unconfigured@null.spigotmc.org>
2021-07-05 19:45:16 +0530

    Added .idea files

--- src/GUI/UIParts.java
+++ src/GUI/UIParts.java
@@ -8,11 +8,8 @@ import java.util.ArrayList;
 public class UIParts {
 
     private ArrayList<JComponent> components;
-    private int width, height;
 
     public UIParts() {
-        this.width = 640;
-        this.height = 480;
         components = new ArrayList<>();
     }
 
@@ -22,13 +19,5 @@ public class UIParts {
 
     public ArrayList<JComponent> getComponents() {
         return components;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
     }
 }
