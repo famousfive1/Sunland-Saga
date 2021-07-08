3b80ad07cbd75802881e50ffcc03665228ee84b9
Dibyam <dibyam1101@gmail.com>
2021-07-08 14:29:31 +0530

    Enemy Generation is Completely Random Now

--- src/States/StateWorld.java
+++ src/States/StateWorld.java
@@ -5,6 +5,8 @@ import Entity.Player;
 import GUI.UIParts;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /*
 Load map
@@ -18,6 +20,7 @@ public class StateWorld extends GameState{
     // Map related stuff
     String[] map;
     JLabel background;
+
 
     public StateWorld() //forestmap1
     {
@@ -36,6 +39,24 @@ public class StateWorld extends GameState{
         map = new String[12]; int j = 0;
         for(String i : Game.loadFile("/assets/Forest1Test.txt"))
             map[j++] = i;
+
+        //Add 10 enemies to the map
+        int i  = 0;
+        while(i!=10){
+            int x = (int)(Math.random()*16 );
+            int y = (int)(Math.random()*12);
+
+            if(map[y].charAt(x)!='1')
+                map[y] = map[y].substring(0, x) + "2" + map[y].substring(x+1, 16);
+            i++;
+        }
+
+        for(int m = 0 ; m<12; m++){
+            for(int n = 0 ; n<16; n++){
+                System.out.print(map[m].charAt(n));
+            }
+            System.out.println();
+        }
     }
 
     @Override
@@ -59,6 +80,6 @@ public class StateWorld extends GameState{
 
     private Character generateEnemy() {
         // Do more stuff
-        return new Character("Enemy", display.loadImg("/assets/PlayerCharacter.png"), 900);
+        return new Character("Enemy", display.loadImg("/assets/PlayerCharacter.png"), (int)(100 +Math.round(Math.random())*900));
     }
 }
