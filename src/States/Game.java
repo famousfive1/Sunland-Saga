49cac3f03fd2014c19990f86dc6ce7c6d5468f25
BuildTools <unconfigured@null.spigotmc.org>
2021-07-08 12:37:00 +0530

    Added a basic file reader function to Game.java

--- src/States/Game.java
+++ src/States/Game.java
@@ -2,6 +2,9 @@ package States;
 
 import GUI.GameWindow;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Game {
 
     static GameWindow window;
@@ -11,6 +14,8 @@ public class Game {
         window = new GameWindow();
         currentState = new StateMainMenu();
         window.setState(currentState);
+        for (String i : loadFile("/assets/forest1Test.txt"))
+            System.out.println(i);
     }
 
     public static GameWindow getWindow() {
@@ -28,5 +33,18 @@ public class Game {
 
     public static void updateWindow() {
         window.updateWindow();
+    }
+
+    public static ArrayList<String> loadFile(String path) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(Game.class.getResourceAsStream(path)));
+        ArrayList<String> s = new ArrayList<>();
+        String st;
+        try {
+            while((st = br.readLine()) != null)
+                s.add(st);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return s;
     }
 }
