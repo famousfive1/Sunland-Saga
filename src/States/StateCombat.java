9871eb0678f50c8464d4dc43baffd80d2a070a51
lynx <76946664+nithin290@users.noreply.github.com>
2021-07-09 21:02:23 +0530

    added health text Area for both player and enemy in StateCombat.java

--- src/States/StateCombat.java
+++ src/States/StateCombat.java
@@ -22,18 +22,35 @@ public class StateCombat extends GameState {
         l.setBounds(100, 100, 100, 100);
         display.addComponent(l);
 
+        JTextArea healthIndicatorPlayer = new JTextArea("Health : " + player.getHealth());
+        healthIndicatorPlayer.setBounds(100, 220, 80, 40);
+        display.addComponent(healthIndicatorPlayer);
+
         l = new JLabel(new ImageIcon(enemy.getImgScaled()));
         l.setBounds(500, 100, 100, 100);
         display.addComponent(l);
 
+        JTextArea healthIndicatorEnemy = new JTextArea("Health : " + enemy.getHealth());
+        healthIndicatorEnemy.setBounds(500, 220, 80, 40);
+        display.addComponent(healthIndicatorEnemy);
+
+
         JButton b = new JButton("Attack 1");
         b.setBounds(200, 400, 80, 40);
-        b.addActionListener(e -> attack('e'));
+        b.addActionListener(e -> {
+            attack('e');
+            healthIndicatorEnemy.setText("Health : " + enemy.getHealth());
+            healthIndicatorPlayer.setText("Health : " + player.getHealth());
+        });
         display.addComponent(b);
 
         b = new JButton("Attack 2");
         b.setBounds(400, 400, 80, 40);
-        b.addActionListener(e -> attack('f'));
+        b.addActionListener(e -> {
+            attack('f');
+            healthIndicatorEnemy.setText("Health : " + enemy.getHealth());
+            healthIndicatorPlayer.setText("Health : " + player.getHealth());
+        });
         display.addComponent(b);
 
         b = new JButton("After player wins, do this");
