1716dbd83721530661792de5772729b3f1b366bd
BuildTools <unconfigured@null.spigotmc.org>
2021-07-09 18:03:46 +0530

    Added a null check to Game.loadFile

--- src/States/Game.java
+++ src/States/Game.java
@@ -35,7 +35,12 @@ public class Game {
     }
 
     public static ArrayList<String> loadFile(String path) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(Game.class.getResourceAsStream(path)));
+        InputStream fs = Game.class.getResourceAsStream(path);
+        if(fs == null) {
+            System.out.println("Couldn't load file at path: " + path);
+            return null;
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(fs));
         ArrayList<String> s = new ArrayList<>();
         String st;
         try {
