303ab83e9a5f819b753e86e9cd7198e6a2f8ccf2
BuildTools <unconfigured@null.spigotmc.org>
2021-07-12 11:15:06 +0530

    Centered dialogue box for NPC

--- src/States/StateCombat.java
+++ src/States/StateCombat.java
@@ -24,7 +24,7 @@ public class StateCombat extends GameState {
         this.player = player;
         this.enemy = enemy;
         JLabel l = new JLabel(new ImageIcon(player.getImgScaled()));
-        l.setBounds(130, 210, 100, 100);
+        l.setBounds(130, 170, 150, 150);
         display.addComponent(l);
 
         healthIndicatorPlayer = new JProgressBar();
@@ -37,7 +37,7 @@ public class StateCombat extends GameState {
         display.addComponent(healthIndicatorPlayer);
 
         l = new JLabel(new ImageIcon(enemy.getImgScaled()));
-        l.setBounds(520, 210, 100, 100);
+        l.setBounds(520, 170, 150, 150);
         display.addComponent(l);
 
         healthIndicatorEnemy = new JProgressBar();
@@ -77,18 +77,18 @@ public class StateCombat extends GameState {
         else System.out.println("Press buttons instead!!");
     }
 
-     void attack(char usedMove){
-        player.reactToMove(usedMove);
-        enemy.takeDamage(200);
+     void attack(char usedMove) {
+         player.reactToMove(usedMove);
+         enemy.takeDamage(200);
 
-         if(usedMove!='f' && enemy.getHealth()!=0)
-             player.takeDamage(10 + (int)(Math.random()*40));
+         if (usedMove != 'f' && enemy.getHealth() != 0)
+             player.takeDamage(10 + (int) (Math.random() * 40));
 
 
-         healthIndicatorEnemy.setValue((enemy.getHealth() * 100)/ Integer.parseInt(fullHealthEnemy));
+         healthIndicatorEnemy.setValue((enemy.getHealth() * 100) / Integer.parseInt(fullHealthEnemy));
          healthIndicatorEnemy.setString(enemy.getHealth() + "/" + fullHealthEnemy);
 
-        //The code below executes 500 ms after the functions call
+         //The code below executes 500 ms after the functions call
          //This is done to add a visual delay in the enemy attack and the player attack
          Timer timer = new Timer(0, e -> {
 
@@ -102,44 +102,38 @@ public class StateCombat extends GameState {
          timer.start();
 
 
-        //The code inside the lambda below will execute 600ms after the function call.
-         //This is done to ensure that the death actually happens sometime after the progress bar gets updated
-        Timer deathCheckTimer = new Timer(0, e->{
+         Timer deathCheckTimer = new Timer(0, e -> {
+             if (enemy.getHealth() == 0) {
+                 System.out.println("Quest Complete");
+                 player.restoreHealth();
+                 save.setQuestCount(save.getQuestCount() - 1);
+                 if (save.getQuestCount() == 0) {
+                     save.setQuestType(-1);
+                     player.setInQuest(false);
+                     Game.setCurrentState(new StateWin());
+                 } else
+                     Game.setCurrentState(save);
 
-        if(enemy.getHealth()==0){
-            System.out.println("Quest Complete");
-            player.restoreHealth();
-            save.setQuestCount(save.getQuestCount() - 1);
-            if(save.getQuestCount()==0) {
-                save.setQuestType(-1);
-                player.setInQuest(false);
-                Game.setCurrentState(new StateWin());
-            }
-            else
-                Game.setCurrentState(save);
+                 save.setQuestDisplay();
+                 save.increaseQuestProgressBar();
+                 //TODO 1. Do something appropriate here
+             } else if (player.getHealth() == 0) {
+                 System.out.println("You Died!!! Sorry!!!!");
+                 player.restoreHealth();
+                 player.decreaseLive();
+                 save.livesDisplay.setText("Lives left " + player.getLives());
 
-            save.setQuestDisplay();
-            save.increaseQuestProgressBar();
-            //TODO 1. Do something appropriate here
-        }
+                 if (player.getLives() == 0) {
+                     System.out.println("You lost the game");
+                     Game.setCurrentState(new StateLost());
+                 } else
+                     Game.setCurrentState(save);
+                 //TODO 2. Do something appropriate here
+             }
+         });
 
-        if(player.getHealth()==0) {
-            System.out.println("You Died!!! Sorry!!!!");
-            player.restoreHealth();
-            player.decreaseLive();
-            save.livesDisplay.setText("Lives left " + player.getLives());
-
-            if(player.getLives() == 0) {
-                System.out.println("You lost the game");
-                Game.setCurrentState(new StateLost());
-            }
-            else
-                Game.setCurrentState(save);
-            //TODO 2. Do something appropriate here
-        }
-
-        deathCheckTimer.setInitialDelay(600);
-        deathCheckTimer.setRepeats(false);
-        deathCheckTimer.start();
-    }
+         deathCheckTimer.setInitialDelay(600);
+         deathCheckTimer.setRepeats(false);
+         deathCheckTimer.start();
+     }
 }
